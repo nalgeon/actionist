c530f4853d91f33544589dc252c4fc7189b6a17a
Anton <m@antonz.org>
2023-02-05 07:40:02 +0500

    regexp benchmark

--- sqlean-re.sql
+++ sqlean-re.sql
@@ -1,6 +1,8 @@
 .load dist/re
-.timer on
 
+select '-- sqlean-re --';
+
+.timer on
 select count(*) from strings where val regexp "1[3-4]";
 select count(*) from strings where val regexp "1[3-4]";
 select count(*) from strings where val regexp "1[3-4]";
--- sqlite-regex.sql
+++ sqlite-regex.sql
@@ -1,6 +1,8 @@
 .load dist/regex0
-.timer on
 
+select '-- sqlite-regex --';
+
+.timer on
 select count(*) from strings where val regexp "1[3-4]";
 select count(*) from strings where val regexp "1[3-4]";
 select count(*) from strings where val regexp "1[3-4]";
